99bd62bad415480d4a4cb4eef779788dce63e75b
Ryan Manny <ryan.w.manny@gmail.com>
2022-06-07 20:58:37 -0700

    Fix Andy page

--- packages/app/src/components/pages/Andy.tsx
+++ packages/app/src/components/pages/Andy.tsx
@@ -1,24 +1,20 @@
-<html>
-<head>
-<title>andy are you okay</title>
-<link rel="stylesheet" type="text/css" href="pagestyle.css">
-<audio autoplay="autoplay" id="rap" loop="loop"> <source src="ofallon/andy.mp3" type="audio/mpeg" /></audio>
-</head>
-<body>
-<div class="right">
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Kid eating dirt -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:250px"
-     data-ad-client="ca-pub-6278343698558743"
-     data-ad-slot="7669651212"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-<a href="index.html">&laquoback</a>
-<br><br>
-<a href="ofallon/ofs_final1.1.zip">DOWNLOAD LINK</a><br>
-<img src="ofallon/andy.png"></img>
+import { Page } from '../Page';
+import { imgPath } from '../helpers';
 
-</body>
+function AndyPage() {
+  return (
+    <Page
+      title="andy are you okay"
+      content={
+        <>
+          <a href="https://github.com/HMcCullough/OFallonStein">link</a>
+          <br/>
+          <img src={imgPath("andy.png")}/>
+        </>
+      }
+      music="andy.mp3"
+    />
+  )
+}
+
+export default AndyPage;
