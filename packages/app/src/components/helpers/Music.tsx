821d845d84e859e1476b3a0f441683eb96234a00
Ryan Manny <ryan.w.manny@gmail.com>
2022-06-07 19:03:36 -0700

    Add music toggle

--- packages/app/src/components/helpers/Music.tsx
+++ packages/app/src/components/helpers/Music.tsx
@@ -1,20 +1,50 @@
+import { useEffect, useState } from 'react';
 
 function musicPath(path: string) {
   return `${process.env['PUBLIC_URL']}/music/${path}`;
 }
+
+const VolumeIcon = () => <>&#128266;</>
+const VolumeMutedIcon = () => <>&#128264;</>
 
 type Props = {
   path: string | undefined;
 }
 
 function Music(props: Props) {
+  const [muted, setMuted] = useState(true);
+
+  useEffect(() => {
+    const audio = document.getElementById('audio') as HTMLAudioElement;
+    if (muted) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+  }, [muted])
+
+  if (!props.path) {
+    return <></>
+  }
+
+  const toggleMute = () => setMuted(!muted);
+
   return (
     <>
-      {props.path && <audio autoPlay loop>
-        <source src={musicPath(props.path)} type="audio/mpeg" />
-      </audio>}
+      <audio id="audio" autoPlay loop muted={muted}>
+        <source
+          src={musicPath(props.path)}
+          type="audio/mpeg"
+        />
+      </audio>
+      <button
+        onClick={toggleMute}
+        style={{ float: 'right' }}
+      >
+        {muted ? <VolumeMutedIcon/> : <VolumeIcon/>}
+      </button>
     </>
-  );
+  )
 }
 
 export default Music;
