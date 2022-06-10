0bbf8bde488daa60b407e90fb6d9715a9e6c6f1f
Ryan Manny <ryan.w.manny@gmail.com>
2022-06-09 22:54:09 -0700

    Clean up and fix Links and Routing

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,36 +1,8 @@
-import {
-  Link,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
+import * as Pages from "./components/pages";
 import { imgPath, Music } from "./components/helpers";
-import {
-  SpoopyPage,
-  AlienPage,
-  AndyPage,
-  BonesawPage,
-  ChallengePage,
-  ChickenPage,
-  DoneskisPage,
-  DunyunRingsPage,
-  FacemanPage,
-  FishingWithDadPage,
-  FrenchPage,
-  HoroscopesPage,
-  JillSandwichPage,
-  DunListPage,
-  LiterallyRatchetPage,
-  MoneyPage,
-  PresidentialSlogansPage,
-  RiddlePage,
-  RipoffPage,
-  SatanClausPage,
-  SongLyricsPage,
-  FeatureSuggestionPage,
-  VladimirSnootinPage,
-  DictionaryPage,
-} from "./components/pages";
+
 
 function HomePage() {
   document.title = "home of the kid eating dirt";
@@ -48,29 +20,29 @@ function HomePage() {
       />
       <br/><br/>
 
-      <Link to="/ripoff">wsu rda calculator</Link>
-      <Link to="/andy">andy are you okay</Link>
-      <Link to="/money">a good many almighty dollar advisories</Link>
-      <Link to="/horoscopes">ignoroscopes </Link>
-      <Link to="/fishingwithdad">fishing with dad</Link>
+      <Link to="/ripoff">wsu rda calculator</Link><br/>
+      <Link to="/andy">andy are you okay</Link><br/>
+      <Link to="/money">a good many almighty dollar advisories</Link><br/>
+      <Link to="/horoscopes">ignoroscopes </Link><br/>
+      <Link to="/fishingwithdad">fishing with dad</Link><br/>
       <br/><br/>
 
-      <Link to="/alien">peaches alien regalia</Link>
-      <Link to="/challenge">clicking challenge</Link>
-      <Link to="/bonesaw">bonesaw is ready</Link>
-      <Link to="/jillsandwich">jill sandwich</Link>
-      <Link to="/words">dictionary useful</Link>
-      <Link to="/satanclaus">satan claus</Link>
-      <Link to="/riddle">difficult riddles!</Link>
-      <Link to="/vladimirsnootin">resident of russia</Link>
-      <Link to="/suggestafeature">suggest a feature</Link>
-      <Link to="/presidentialslogans">presidential campaign slogans</Link>
-      <Link to="/listofdun">list of dun</Link>
-      <Link to="/dunyunrings">we're dunyun rings</Link>
-      <Link to="/2spoopy">2spoopy4me</Link>
-      <Link to="/faceman">face the truth</Link>
-      <Link to="/chicken">chicken</Link>
-      <Link to="/songlyrics">song lyrics</Link>
+      <Link to="/alien">peaches alien regalia</Link><br/>
+      <Link to="/challenge">clicking challenge</Link><br/>
+      <Link to="/bonesaw">bonesaw is ready</Link><br/>
+      <Link to="/jillsandwich">jill sandwich</Link><br/>
+      <Link to="/words">dictionary useful</Link><br/>
+      <Link to="/satanclaus">satan claus</Link><br/>
+      <Link to="/riddle">difficult riddles!</Link><br/>
+      <Link to="/vladimirsnootin">resident of russia</Link><br/>
+      <Link to="/suggestafeature">suggest a feature</Link><br/>
+      <Link to="/presidentialslogans">presidential campaign slogans</Link><br/>
+      <Link to="/listofdun">list of dun</Link><br/>
+      <Link to="/dunyunrings">we're dunyun rings</Link><br/>
+      <Link to="/2spoopy">2spoopy4me</Link><br/>
+      <Link to="/faceman">face the truth</Link><br/>
+      <Link to="/chicken">chicken</Link><br/>
+      <Link to="/songlyrics">song lyrics</Link><br/>
       <br/><br/>
 
       <a href="https://github.com/ryanmanny">my github</a>
@@ -86,7 +58,7 @@ function HomePage() {
   )
 }
 
-function App() {
+export default function App() {
   return (
     <div
       className="App"
@@ -99,33 +71,32 @@ function App() {
     >
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/2spoopy" element={<SpoopyPage />} />
-        <Route path="/alien" element={<AlienPage />} />
-        <Route path="/andy" element={<AndyPage />} />
-        <Route path="/bonesaw" element={<BonesawPage />} />
-        <Route path="/challenge" element={<ChallengePage />} />
-        <Route path="/chicken" element={<ChickenPage />} />
-        <Route path="/doneskis" element={<DoneskisPage />} />
-        <Route path="/dunyunrings" element={<DunyunRingsPage />} />
-        <Route path="/faceman" element={<FacemanPage />} />
-        <Route path="/fishingwithdad" element={<FishingWithDadPage />} />
-        <Route path="/french" element={<FrenchPage />} />
-        <Route path="/horoscopes" element={<HoroscopesPage />} />
-        <Route path="/jillsandwich" element={<JillSandwichPage />} />
-        <Route path="/listofdun" element={<DunListPage />} />
-        <Route path="/literallyratchet" element={<LiterallyRatchetPage />} />
-        <Route path="/money" element={<MoneyPage />} />
-        <Route path="/presidentialslogans" element={<PresidentialSlogansPage />} />
-        <Route path="/riddle" element={<RiddlePage />} />
-        <Route path="/ripoff" element={<RipoffPage />} />
-        <Route path="/satanclaus" element={<SatanClausPage />} />
-        <Route path="/songlyrics" element={<SongLyricsPage />} />
-        <Route path="/suggestafeature" element={<FeatureSuggestionPage />} />
-        <Route path="/vladimirsnootin" element={<VladimirSnootinPage />} />
-        <Route path="/words" element={<DictionaryPage />} />
+
+        {/* <Route path="/2spoopy" element={<Pages.SpoopyPage />} /> */}
+        <Route path="/alien" element={<Pages.AlienPage />} />
+        <Route path="/andy" element={<Pages.AndyPage />} />
+        <Route path="/bonesaw" element={<Pages.BonesawPage />} />
+        <Route path="/challenge" element={<Pages.ChallengePage />} />
+        {/**<Route path="/chicken" element={<Pages.ChickenPage />} /> */}
+        {/**<Route path="/doneskis" element={<Pages.DoneskisPage/>}/> */}
+        {/**<Route path="/dunyunrings" element={<Pages.DunyunRingsPage/>}/> */}
+        {/**<Route path="/faceman" element={<Pages.FacemanPage/>}/> */}
+        {/**<Route path="/fishingwithdad" element={<Pages.FishingWithDadPage/>}/> */}
+        {/**<Route path="/french" element={<Pages.FrenchPage/>}/> */}
+        {/**<Route path="/horoscopes" element={<Pages.HoroscopesPage/>}/> */}
+        {/**<Route path="/jillsandwich" element={<Pages.JillSandwichPage/>}/> */}
+        {/**<Route path="/listofdun" element={<Pages.DunListPage/>}/> */}
+        {/**<Route path="/literallyratchet" element={<Pages.LiterallyRatchetPage/>}/> */}
+        {/**<Route path="/money" element={<Pages.MoneyPage/>}/> */}
+        {/**<Route path="/presidentialslogans" element={<Pages.PresidentialSlogansPage/>}/> */}
+        {/**<Route path="/riddle" element={<Pages.RiddlePage/>}/> */}
+        {/**<Route path="/ripoff" element={<Pages.RipoffPage/>}/> */}
+        {/**<Route path="/satanclaus" element={<Pages.SatanClausPage/>}/> */}
+        {/**<Route path="/songlyrics" element={<Pages.SongLyricsPage/>}/> */}
+        {/**<Route path="/suggestafeature" element={<Pages.FeatureSuggestionPage/>}/> */}
+        {/**<Route path="/vladimirsnootin" element={<Pages.VladimirSnootinPage/>}/> */}
+        {/**<Route path="/words" element={<Pages.DictionaryPage/>}/> */}
       </Routes>
     </div>
   );
 }
-
-export default App;
