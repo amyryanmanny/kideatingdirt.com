c75110572ffee193e808fefff3173b37730676ae
Ryan Manny <ryan.w.manny@gmail.com>
2022-06-10 20:33:13 -0700

    Reorder media exports

--- packages/app/src/components/media/index.ts
+++ packages/app/src/components/media/index.ts
@@ -1,4 +1,7 @@
-export { default as Music } from "./Music";
+export const imgPath = (path: string) =>
+  `${process.env['PUBLIC_URL']}/pic/${path}`
 
-export const imgPath = (path: string) => `${process.env['PUBLIC_URL']}/pic/${path}`
-export const musicPath = (path: string) => `${process.env['PUBLIC_URL']}/music/${path}`
+export const musicPath = (path: string) =>
+  `${process.env['PUBLIC_URL']}/music/${path}`
+
+export { default as Music } from "./Music";
